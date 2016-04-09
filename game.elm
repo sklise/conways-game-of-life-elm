75dfbe9fef61efa4a4c62cda752edb7808157646
Steve <sk@sklise.com>
2016-04-09 09:54:01 -0400

    rendering and advancing, but flawed

--- game.elm
+++ game.elm
@@ -1,6 +1,8 @@
+import Array
 import Debug
 import Graphics.Element exposing (show)
-import Array
+import Html exposing (..)
+import Time exposing (..)
 
 leftIndex: Int -> Int -> Int
 leftIndex i w =
@@ -37,22 +39,74 @@ neighborCount board width height =
     |> List.map justToInt
     |> List.sum)
 
-width = 4
-height = 4
-board = Array.fromList [0,0,0,0,
-                        0,1,1,0,
-                        0,1,1,0,
-                        0,0,0,0]
-
-neighborsToNewGen list =
+neighborsToNewGen: Array.Array Int -> Array.Array Int
+neighborsToNewGen arr =
   Array.indexedMap (\i n ->
-    if n == 4
-    then justToInt (Array.get i board)
-    else if n == 3
-    then 1
-    else 0) list
+    if n == 4 then
+      justToInt (Array.get i arr)
+    else if n == 3 then
+      1
+    else 0) arr
+
+type alias World =
+  { w: Int
+  , h: Int
+  , b: Array.Array Int
+  }
+
+worldCensus: World -> World
+worldCensus world =
+  { w = world.w
+  , h = world.h
+  , b = Array.indexedMap (\n _ -> (neighborCount world.b world.w world.h) n) world.b
+      |> neighborsToNewGen
+  }
+
+-- World Update
+
+updateWorld : Input -> World -> World
+updateWorld {delta} model =
+  worldCensus model
+
+-- View
+
+view : World -> Html
+view model =
+  div []
+    [
+    div [] (List.map (\n -> text (toString n)) (Array.toList model.b))
+    ]
+
+
+defaultWorld : World
+defaultWorld =
+  { w = 5
+  , h = 5
+  , b = Array.fromList [1,0,0,0,0,
+                        0,1,1,0,1,
+                        0,1,1,0,0,
+                        0,0,0,0,1,
+                        1,0,0,1,0]
+  }
+
+delta =
+  Signal.map inSeconds (fps 1)
+
+type alias Input =
+  { delta : Time
+  }
+
+input : Signal Input
+input =
+  Signal.sampleOn delta <|
+    Signal.map Input
+      delta
+
+worldState : Signal World
+worldState =
+  Signal.foldp updateWorld defaultWorld input
 
 main =
-  Array.indexedMap (\n _ -> (neighborCount board width height) n) board
-    |> neighborsToNewGen
-    |> show
+  Signal.map view worldState
+
+
