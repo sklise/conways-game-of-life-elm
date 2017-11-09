6fec14ee9ade66a9bab45ea7f446c2993c21f9bc
Steve <sk@sklise.com>
2017-11-09 12:06:11 -0500

    working with dicts

--- game-of-life.elm
+++ game-of-life.elm
@@ -1,157 +1,145 @@
 module Main exposing (..)
 
-import Array
+import Dict exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Random
 import Svg exposing (Svg)
 import Svg.Attributes
 import Time exposing (Time, millisecond)
 
 
-leftIndex : Int -> Int -> Int
-leftIndex i w =
-    if (i % w) == 0 then
-        i - 1 + w
-    else
-        i - 1
-
-
-rightIndex : Int -> Int -> Int
-rightIndex i w =
-    if (i + 1) % w == 0 then
-        i + 1 - w
-    else
-        i + 1
-
-
-buildIndices : Int -> Int -> Int -> List Int
-buildIndices i w h =
-    let
-        t =
-            w * h
-
-        up =
-            (i - w) % t
-
-        down =
-            (i + w) % t
-    in
-        [ (leftIndex up w)
-        , up
-        , (rightIndex up w)
-        , (leftIndex i w)
-        , i
-        , (rightIndex i w)
-        , (leftIndex down w)
-        , down
-        , (rightIndex down w)
-        ]
-
-
-getNeighbors : List Int -> Array.Array number -> List number
-getNeighbors indices cells =
-    List.map (\n -> Array.get n cells |> Maybe.withDefault 0) indices
-
-
-countNeighbors : List number -> Int -> Int -> Int -> number
-countNeighbors board width height =
-    let
-        boardArr =
-            Array.fromList board
-    in
-        (\n ->
-            boardArr
-                |> getNeighbors (buildIndices n width height)
-                |> List.sum
-        )
-
-
-neighborsToNewGen : Int -> Int -> Int
-neighborsToNewGen boardVal neighborCount =
-    if neighborCount == 4 then
-        boardVal
-    else if neighborCount == 3 then
-        1
-    else
-        0
-
-
-worldCensus : World -> World
-worldCensus world =
-    let
-        listBoard =
-            Array.fromList world.b
-
-        nc =
-            Array.toList (Array.indexedMap (\n _ -> (countNeighbors world.b world.w world.h) n) listBoard)
-    in
-        { world
-            | b = (List.map2 neighborsToNewGen world.b nc)
-        }
-
-
-cellToDiv : Int -> Int -> Int -> Int -> Int -> Svg a
-cellToDiv width height cellSize index cell =
-    let
-        x =
-            (index % width)
-                |> (*) cellSize
-                |> toString
-
-        y =
-            (index // width)
-                |> (*) cellSize
-                |> toString
-
-        fillColor =
-            if cell == 1 then
-                "black"
-            else
-                "white"
-    in
-        Svg.rect
-            [ Svg.Attributes.width (toString cellSize)
-            , Svg.Attributes.height (toString cellSize)
-            , Svg.Attributes.x x
-            , Svg.Attributes.y y
-            , Svg.Attributes.fill fillColor
-            ]
-            []
-
-
-renderWorld : World -> List (Html a)
-renderWorld world =
-    (List.indexedMap (cellToDiv world.w world.h world.cellSize) world.b)
-
-
-
--- MODEL
--- World is an array of cell states and dimensions of the world
-
-
-type alias World =
-    { w : Int
-    , h : Int
-    , cellSize : Int
-    , b : List Int
+type alias Cell =
+    { x : Int
+    , y : Int
     }
 
 
-init : Int -> Int -> Int -> ( World, Cmd Msg )
-init w h cellSize =
-    ( World w h cellSize (List.range 0 (w * h))
-    , Random.generate Chill (Random.list (w * h) (Random.int 0 1))
-    )
+type alias CellWithNeighbors =
+    { x : Int
+    , y : Int
+    , n : Int
+    , s : Bool
+    }
 
 
+type alias World =
+    Dict String Cell
 
--- UPDATE
+
+type alias WorldNeighbors =
+    Dict String CellWithNeighbors
 
 
 type Msg
     = Advance Time
-    | Chill (List Int)
+    | Chill World
+
+
+coordToKey : Int -> Int -> String
+coordToKey x y =
+    (toString x) ++ "/" ++ (toString y)
+
+
+init : World -> ( World, Cmd Msg )
+init world =
+    ( world, Cmd.none )
+
+
+incNeighborCount : Int -> Int -> Maybe CellWithNeighbors -> Maybe CellWithNeighbors
+incNeighborCount x y i =
+    case i of
+        Nothing ->
+            Just { y = y, x = x, s = False, n = 1 }
+
+        Just i ->
+            Just { i | n = (i.n + 1) }
+
+
+updateSelf : Int -> Int -> Maybe CellWithNeighbors -> Maybe CellWithNeighbors
+updateSelf x y cell =
+    case cell of
+        Nothing ->
+            Just { y = y, x = x, s = True, n = 0 }
+
+        Just cell ->
+            Just { cell | s = True }
+
+
+worldToNeighborCount : a -> Cell -> Dict String CellWithNeighbors -> Dict String CellWithNeighbors
+worldToNeighborCount key cell neighbors =
+    let
+        x =
+            cell.x
+
+        y =
+            cell.y
+
+        n0 =
+            Dict.update (coordToKey x y) (updateSelf x y) neighbors
+
+        n1 =
+            Dict.update (coordToKey (x - 1) y) (incNeighborCount (x - 1) y) n0
+
+        n2 =
+            Dict.update (coordToKey (x - 1) (y + 1)) (incNeighborCount (x - 1) (y + 1)) n1
+
+        n3 =
+            Dict.update (coordToKey (x - 1) (y - 1)) (incNeighborCount (x - 1) (y - 1)) n2
+
+        n4 =
+            Dict.update (coordToKey x (y - 1)) (incNeighborCount x (y - 1)) n3
+
+        n5 =
+            Dict.update (coordToKey x (y + 1)) (incNeighborCount x (y + 1)) n4
+
+        n6 =
+            Dict.update (coordToKey (x + 1) y) (incNeighborCount (x + 1) y) n5
+
+        n7 =
+            Dict.update (coordToKey (x + 1) (y - 1)) (incNeighborCount (x + 1) (y - 1)) n6
+
+        n8 =
+            Dict.update (coordToKey (x + 1) (y + 1)) (incNeighborCount (x + 1) (y + 1)) n7
+    in
+        n8
+
+
+killSpawnLive :
+    String
+    -> CellWithNeighbors
+    -> Dict String Cell
+    -> Dict String Cell
+killSpawnLive key entry newDict =
+    if (not entry.s && entry.n == 3) then
+        Dict.insert key { x = entry.x, y = entry.y } newDict
+    else if entry.s && (entry.n == 2 || entry.n == 3) then
+        Dict.insert key { x = entry.x, y = entry.y } newDict
+    else
+        newDict
+
+
+worldCensus : World -> World
+worldCensus dict =
+    dict
+        |> Dict.foldl worldToNeighborCount Dict.empty
+        |> Dict.foldl killSpawnLive Dict.empty
+
+
+renderCell : Cell -> Svg msg
+renderCell cell =
+    Svg.rect
+        [ Svg.Attributes.width "10"
+        , Svg.Attributes.height "10"
+        , Svg.Attributes.stroke "white"
+        , Svg.Attributes.x (toString (cell.x * 10 + 500))
+        , Svg.Attributes.y (toString (cell.y * 10 + 500))
+        , Svg.Attributes.fill "black"
+        ]
+        []
+
+
+
+-- UPDATE
 
 
 update : Msg -> World -> ( World, Cmd Msg )
@@ -160,8 +148,8 @@ update msg world =
         Advance newTime ->
             ( worldCensus world, Cmd.none )
 
-        Chill l ->
-            ( { world | b = l }, Cmd.none )
+        Chill w ->
+            ( w, Cmd.none )
 
 
 
@@ -170,7 +158,7 @@ update msg world =
 
 subscriptions : World -> Sub Msg
 subscriptions world =
-    Time.every (millisecond * 1000) Advance
+    Time.every (millisecond * 500) Advance
 
 
 
@@ -179,17 +167,31 @@ subscriptions world =
 
 view : World -> Html Msg
 view world =
-    let
-        renderWidth = world.w * world.cellSize
-        renderHeight = world.h * world.cellSize
-    in
-        Svg.svg [ width renderWidth, height renderHeight ] (renderWorld world)
+    Svg.svg
+        [ width 1000
+        , height 1000
+        ]
+        (List.map renderCell (Dict.values world))
+
+
+newWorld : List ( String, Cell ) -> World
+newWorld l =
+    Dict.fromList l
 
 
 main : Program Never World Msg
 main =
     Html.program
-        { init = init 60 60 10
+        { init =
+            ( newWorld
+                [ ( "0/0", { x = 0, y = 0 } )
+                , ( "1/0", { x = 1, y = 0 } )
+                , ( "-1/1", { x = -1, y = 1 } )
+                , ( "0/1", { x = 0, y = 1 } )
+                , ( "0/2", { x = 0, y = 2 } )
+                ]
+            , Cmd.none
+            )
         , view = view
         , update = update
         , subscriptions = subscriptions
